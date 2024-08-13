a278e24cb40fe5d3218d9222a5c12b5203522ddf
loomoni <loomonimorwo1@gmail.com>
2024-08-13 11:05:07 +0300

    changed to checked by wages submitted ss

--- wages/__manifest__.py
+++ wages/__manifest__.py
@@ -19,12 +19,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mail','account','hr'],
+    'depends': ['base', 'mail', 'account', 'hr'],
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
         'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'data/report.xml',
